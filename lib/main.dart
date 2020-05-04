914258757af4211087c40336966ffbc90c5bc27a
zhangzeshuan <zhangzeshuan@ifanr.com>
2020-05-04 10:57:06 +0800

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,72 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: changeDiceFace,
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: changeDiceFace,
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+// class DicePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Row(
+//           children: <Widget>[
+//             Expanded(
+//               child: FlatButton(
+//                 onPressed: () {
+//                   print('Left button got pressed.');
+//                 },
+//                 child: Image.asset('images/dice1.png'),
+//               ),
+//             ),
+//             Expanded(
+//               child: FlatButton(
+//                 onPressed: () {
+//                   print('Right button got pressed.');
+//                 },
+//                 child: Image.asset('images/dice1.png'),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
